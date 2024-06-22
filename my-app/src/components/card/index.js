706830ff5b591ddc5bcd7deaff7ca40287d163a5
Karen015 <karen.aghajanyan.11@gmail.com>
2024-06-22 18:00:22 +0400

    implement card and button functionality

--- my-app/src/components/card/index.js
+++ my-app/src/components/card/index.js
@@ -1,14 +1,32 @@
 import React from "react";
 
 class Card extends React.Component{
-    // constructor() {
+    state = {
+        count: 0
+    }
+    increment = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    decrement = () => {
+        this.setState({
+            count: this.state.count - 1
+        })
 
-    // }
+    }
     render() {
+        console.log(this)
+        
         return (
             <div className="card_container">
-                <h2>Card</h2>
+                <h2>{this.props.title}</h2>
+                <button onClick={this.decrement} disabled={this.state.count === 0}>-</button>
+                    <span>count {this.state.count}</span>
+                <button onClick={this.increment} disabled={this.state.count >= 5}>+</button>
             </div>
+            
+            
         )
     }
 };
